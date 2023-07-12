4a7bedeeadbacde7b641a4c4e2c904d9c7f1758c
mydomian <mortuza504@gmail.com>
2023-07-12 15:57:54 +0600

    Jet stream Setup

---
+++ database/migrations/2023_07_12_074332_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('sub_sub_category_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('title');
+            $table->string('image');
+            $table->float('price');
+            $table->longText('description');
+            $table->string('words');
+            $table->bigInteger('views')->default(0);
+            $table->bigInteger('favourites')->default(0);
+            $table->enum('is_tips',['yes','no']);
+            $table->enum('is_tested',['yes','no']);
+            $table->enum('is_hq_images',['yes','no']);
+            $table->enum('status',['active','inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
