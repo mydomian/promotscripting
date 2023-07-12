3a4bc8a0e2dd5c3594511f2aa5170cdee873999f
mydomian <mortuza504@gmail.com>
2023-07-12 19:07:27 +0600

    Admin

--- app/Http/Controllers/Admin/Auth/AuthController.php
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function login(Request $request){
-
         if($request->isMethod('post')){
             $request->validate(['email' => 'required|email','password' => 'required']);
             if(Auth::attempt(['email'=>$request->email,'password'=>$request->password,'is_admin'=>'admin'])){
