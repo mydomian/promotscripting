5128749e5f1870cda3a052d57ce0d15097bc612a
ZIllur Imran <zillurimran@gmail.com>
2023-07-24 16:53:10 +0600

    transaction module in admin panel | purchase | sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -9,4 +9,19 @@ class Sale extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+
+
+    public function order(){
+        return $this->belongsTo(Order::class,'order_id','id');
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class)->with('subSubCategory');
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
 }
